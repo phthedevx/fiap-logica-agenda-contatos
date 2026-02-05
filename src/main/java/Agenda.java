bf562e6e9820ad86f7e99c0e979b622e8a9bf3a8
Pedro Henrique <phthedevx@gmail.com>
2026-02-05 17:37:38 -0300

    Adicionando classe agenda e contato

---
+++ src/main/java/Agenda.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agenda {
+    public List<Contato> c = new ArrayList<>();
+
+
+}
---
+++ src/main/java/Contato.java
@@ -0,0 +1,35 @@
+public class Contato {
+    private String nome;
+    private String numero;
+    private String email;
+
+    public Contato(String nome, String numero, String email) {
+        this.nome = nome;
+        this.numero = numero;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
